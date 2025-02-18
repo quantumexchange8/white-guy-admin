1d126c505f734b420d8c8a79f31c8fc2fd806853
unknown <demondrake00@gmail.com>
2025-02-18 10:44:54 +0800

    manually set csrf token

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,12 @@
 import axios from 'axios';
 window.axios = axios;
 
+// Get CSRF token from meta tag
+const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+// Set CSRF token to Axios headers if it exists
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token;
+}
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="robots" content="noindex, nofollow">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         @if (App::environment('production'))
             <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         @endif
